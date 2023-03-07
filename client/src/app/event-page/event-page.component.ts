6528e71911c3b7030fbac918e0b222c2411f60fb
Amruta1311 <amruta13mulay11@gmail.com>
2023-03-06 18:51:59 -0800

    Restructuring of files

---
+++ client/src/app/event-page/event-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-page',
+  templateUrl: './event-page.component.html',
+  styleUrls: ['./event-page.component.css']
+})
+export class EventPageComponent implements OnInit {
+
+  defaultDistance : number = 10;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+
+  
+
+}
