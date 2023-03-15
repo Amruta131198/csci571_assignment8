f137eae37961ed593344106eb82a702165996d1b
Amruta1311 <amruta13mulay11@gmail.com>
2023-03-14 17:04:01 -0700

    Added diable/enable location

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { MatInputModule } from '@angular/material/input';
     MatAutocompleteModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule, 
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/event-page/event-page.component.ts
+++ client/src/app/event-page/event-page.component.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core'
-import { FormControl } from '@angular/forms'
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { Observable } from 'rxjs'
 import { map, startWith } from 'rxjs/operators'
 
@@ -11,19 +11,11 @@ import { map, startWith } from 'rxjs/operators'
 })
 export class EventPageComponent implements OnInit {
 
-  constructor(private http: HttpClient) { }
-
-  ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value => this.serverCallForAutoComplete(value))
-    );
-
-    console.log("Filtered Options : " + this.filteredOptions)
-  }
+  constructor(private http : HttpClient, private fb : FormBuilder) { }
 
   defaultDistance : number = 10;
   public PARENT_SERVER_URL : String = 'http://localhost:3000';
+  public IP_INFO_URL = 'https://ipinfo.io/?token=18175a85885dbc';
 
   // Stores auto-complete content
   public choices : string[] = []; 
@@ -31,39 +23,100 @@ export class EventPageComponent implements OnInit {
 
   public myControl = new FormControl();
 
+  public eventForm  = new FormGroup({
+      keyword: new FormControl(),
+      distance: new FormControl(),
+      category: new FormControl(),
+      location: new FormControl(),
+      locationCheckbox : new FormControl()
+    });
+
+  public autoDetect = false;
+
   public formInputValue : any = {
     keyword : '',
     distance : '',
-    category : '',
-    location : ''
+    category : 'Default',
+    location : '',
+    latitude : '',
+    longitude : ''
+  }
+
+  ngOnInit() {
+
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this.serverCallForAutoComplete(value))
+    );
   }
 
   serverCallForAutoComplete(key : string): string[]
   {
     var SERVER_URL = this.PARENT_SERVER_URL + '/autocomplete?keyword=' + key;
     console.log("Auto-complete backendUrl:", SERVER_URL);
-    const filterValue = key.toLowerCase()
-    fetch(SERVER_URL)
-      .then(result => result.json())
-      .then(result => {
-        var autoCompleteResponseList = [];
-        for(var i = 0 ; i < result.attractions.length ; i++)
-        {
-          autoCompleteResponseList.push(result.attractions[i].name);
-        }
-        this.choices = autoCompleteResponseList;
-      })
-
-      console.log("Choices: "+this.choices);
+    if(key != '' || key != null)
+    {
+      const filterValue = key.toLowerCase();
+      fetch(SERVER_URL)
+        .then(result => result.json())
+        .then(result => {
+          var autoCompleteResponseList = [];
+          for(var i = 0 ; i < result.attractions.length ; i++)
+          {
+            autoCompleteResponseList.push(result.attractions[i].name);
+          }
+          this.choices = autoCompleteResponseList;
+        })
+  
+        console.log("Choices: "+this.choices);
+    }
       return this.choices;
-      // .filter(option =>
-      //   option.toLowerCase().includes(filterValue)
-      // );
   }
 
   displayFn(subject : string) {
-    console.log(subject)
+    // console.log(subject)
     return subject;
+  }
+
+  public fetchLocation(key : any)
+  {
+    this.autoDetect = !this.autoDetect;
+    console.log("AutoDetect : " + this.autoDetect);
+
+    this.myControl.get('location')?.disable;
+
+    if(this.autoDetect == true)
+    {
+      this.eventForm.get('location')?.disable();
+      fetch(this.IP_INFO_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        this.formInputValue.location = data?.city;
+
+        var coordinates : String = data?.loc;
+
+        this.formInputValue.latitude = coordinates.substring(0,7);
+        this.formInputValue.longitude = coordinates.substring(8,coordinates.length);
+        this.formInputValue.location = "";
+
+        console.log("Latitude : " + this.formInputValue.latitude + " Longitude : " + this.formInputValue.longitude);
+        });
+    }
+    else
+    {
+      this.eventForm.get('location')?.enable();
+      this.formInputValue.location = '';
+      this.formInputValue.latitude = '';
+      this.formInputValue.longitude = '';
+      console.log("User Input Location : " + this.formInputValue.location);      
+    }
+  }
+
+  public onSubmit(form : any)
+  {
+    console.log("Submitted!!!!");
+    console.log("Keyword : " + this.formInputValue.keyword + " , distance : " 
+    + this.defaultDistance + ", category : " + this.formInputValue.category + ' , location : ' + this.formInputValue.location);
   }
 
 }
