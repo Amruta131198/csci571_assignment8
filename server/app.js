47a3f0c88482065663aefd18b7050db00e712244
Amruta1311 <amruta13mulay11@gmail.com>
2023-03-21 20:27:21 -0700

    Added albums from spotify

--- server/app.js
+++ server/app.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const app = express();
-
+var qs = require('qs');
 var geohash = require('ngeohash');
-
 const axios = require('axios');
 const util = require('util');
 var spotifyWebApi = require('spotify-web-api-node');
 const res = require("express/lib/response");
 const SpotifyWebApi = require("spotify-web-api-node");
+var data = qs.stringify({
+    'grant_type': 'client_credentials' 
+  });
 
 // Client ID : 657a16b519154096bd1b8e53e8659b8e
 // Client Secret : 1dd2a6650aa145cc91ee28251a535a90
 
-//&keyword=USC&segmentId=KZFzniwnSyZfZ7v7nE&radius=10&unit=miles&geoPoint=9q5cs
+var clientId = '657a16b519154096bd1b8e53e8659b8e';
+var clientSecret = '1dd2a6650aa145cc91ee28251a535a90';
+
+// //&keyword=USC&segmentId=KZFzniwnSyZfZ7v7nE&radius=10&unit=miles&geoPoint=9q5cs
 
 app.get('/events', (request, response) => {
 
@@ -188,6 +193,98 @@ app.get('/spotify', (request, response) => {
 );
 });
 
+app.get('/spotifyAlbum', async (request, response) => {
+
+    console.log(":::: REQUEST QUERY ====> " + request.query.artistId);
+    var id = request.query.artistId;
+
+    var searchURL = "https://api.spotify.com/v1/artists/"+ id+ "/albums";
+
+    var c = {
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        headers: {
+            'Authorization': `Basic ${Buffer.from(clientId + ':' + clientSecret).toString('base64')}`,
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        data : qs.stringify({ 'grant_type': 'client_credentials'  })
+    };
+
+    axios(c)
+        .then(function (res) {
+            console.log("RESPONSE ::::"+JSON.stringify(res.data));
+
+            axios.get(searchURL, {
+                headers : {
+                    'Authorization' : `Bearer ${res.data.access_token}`
+                },
+                params: {
+                    limit: 3
+                }
+            })
+                .then((resAxios) => {
+                    console.log("Result for Albums : " + JSON.stringify(resAxios.data));
+                    response.header("Access-Control-Allow-Origin", "*");
+                    response.send(JSON.stringify(resAxios.data));
+                    console.log("Spotify Artists Response Sent!");
+                })
+                .catch(err =>{
+                    console.log(err);
+                });
+        })
+        .catch(function (error) {
+            console.log('error', error);
+        });
+});
+
+
+
+//Search Artist's Album in Spotify API call
+// app.get('/spotifyAlbum', (request, response) => {
+
+//     console.log(":::: REQUEST QUERY ====> " + request.query.artistId);
+
+//     var id = request.query.artistId;
+
+//     var SPOTIFY_API_CALL = new SpotifyWebApi({
+//         clientId: '657a16b519154096bd1b8e53e8659b8e',
+//         clientSecret: '1dd2a6650aa145cc91ee28251a535a90'
+//     });
+
+//     SPOTIFY_API_CALL.clientCredentialsGrant()
+//     .then(
+//             function(data)
+//             {
+//                 console.log("Access Token : " + data.body['access_token']);
+
+//                 SPOTIFY_API_CALL.setAccessToken(data.body['access_token']);
+
+//                 SPOTIFY_API_CALL.getArtistAlbums(id,limit=3)
+//                         .then(
+//                             function(data)
+//                             {
+//                                 console.log(":: ARTIST'S ALBUM :: " + data.body);
+
+//                                 response.header("Access-Control-Allow-Origin", "*");
+//                                 response.send(JSON.stringify(data.body));
+//                                 console.log("Spotify Artist's Album Response Sent!");
+//                             },
+
+//                             function(error)
+//                             {
+//                                 console.log("Error Message when fetching Spotify API : " + error);
+//                             }
+//                         );
+//                 },
+
+//                 function(error)
+//                 {
+//                     console.log("Error Message when fetching Spotify API : " + error);
+//                 }
+//                 );
+// });
+
+
 // Event Details API call
 app.get('/eventDetails', (request, response) => {
 
@@ -247,3 +344,8 @@ app.get('/venueDetail', (request, response) => {
 app.listen(3000, (req, res) => {
     console.log("Express API is running on Port 3000!");
 })
+
+
+//BQC-wvQNK1L27K8KZD6HMonK38_BSy_EuhY4rC_YoiUr3mY6_QeAGKuo-C9ly6QYExc8zIwu9yhL6unr5Mey3UJ-R9MYmi9kVSlFUeg4G_yiYhIqKgQn
+
+//curl -X "GET" "https://api.spotify.com/v1/artists/2cFrymmkijnjDg9SS92EPM/albums" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQDGLmyKw-VSbKjVCIKk_WLbwMpc-rsy6YYBQ1KN40b_ILG5tKSV9u6qK39ckqrayar2eWxIBH_bgTLmp0BmyeIUZ8JGeibzowM2mnCqXuYtmWi76zuX"
